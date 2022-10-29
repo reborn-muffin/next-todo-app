5990188c1b9f6374f1cbfe91f0f03fb38762288f
Bohdan Datsenko <reborn.muffin@gmail.com>
2022-10-29 13:30:26 -0700

    add theme for App component

---
+++ src/app/App.js
@@ -0,0 +1,21 @@
+import {Provider} from "react-redux";
+import {store} from "../redux-store/store";
+import {Layout} from "../layout/Layout";
+import {ThemeProvider} from "@mui/material"
+import appTheme from "./appTheme"
+
+function App() {
+  return (
+      <Provider store={store}>
+        <ThemeProvider theme={appTheme}>
+            <div className="App">
+                <header className="App-header">
+                    <Layout />
+                </header>
+            </div>
+        </ThemeProvider>
+      </Provider>
+  );
+}
+
+export default App;
---
+++ src/app/appTheme.js
@@ -0,0 +1,12 @@
+import {createTheme} from "@mui/material"
+
+export default createTheme({
+    palette: {
+        primary: {
+            main: "#673AB7"
+        },
+        secondary:{
+            main: "#757575"
+        },
+    }
+})
