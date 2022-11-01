f7c24f339ae44d4b1cfc467dabce2acda9252a3d
Bohdan Datsenko <reborn.muffin@gmail.com>
2022-11-01 15:14:55 -0700

    add list with task

---
+++ src/taskList/TaskList.js
@@ -0,0 +1,28 @@
+import {useEffect} from "react"
+import {loadTasks} from "./TaskAPI"
+import {useDispatch, useSelector} from "react-redux"
+import { Card, CardContent, Grid, Typography} from "@mui/material"
+import {styles} from "./styles"
+
+export const TaskList = () => {
+    const customStyles = styles();
+    const dispatch = useDispatch()
+    const tasks = useSelector(state => state.tasks.tasks)
+
+    useEffect(() => {
+        dispatch(loadTasks())
+    }, [])
+
+    const taskCard = (task) => (<Grid item flexGrow={1} style={customStyles.taskGrid}>
+        <Card key={task.id} style={{height: 250}}>
+            <CardContent>
+                <Typography variant={"h4"}>{task.title}</Typography>
+                <Typography variant={"body1"} style={customStyles.taskBody}>{task.description}</Typography>
+            </CardContent>
+        </Card>
+    </Grid>)
+
+    return <Grid container spacing={5} style={customStyles.rootGrid}>
+        {tasks && tasks.map(taskCard)}
+    </Grid>
+}
