a3b4ccbc5a66c69e28e492e8313af32ce4deb930
Bohdan Datsenko <reborn.muffin@gmail.com>
2022-11-14 12:59:44 -0800

    add mobile support for alerts

--- src/common/alerts/AlertManager.js
+++ src/common/alerts/AlertManager.js
@@ -1,18 +1,23 @@
 import {useDispatch, useSelector} from "react-redux"
-import alertStyles from "./alertStyles"
+import useAlertStyles from "./alertStyles"
 import {removeAlert} from "./AlertSlice"
 import AutoHideAlert from "./AutoHideAlert"
+import {useTheme} from "@mui/styles"
+import {Box} from "@mui/material"
 
 const AlertManager = () => {
     const dispatch = useDispatch()
     const alertItems = useSelector(state => state.alerts.items)
+
+    const theme = useTheme()
+    const customStyles = useAlertStyles(theme)
 
     const makeAlert = (item) => <AutoHideAlert handleClose={() => dispatch(removeAlert(item.id))}
                                  message={item.message} severity={item.severity} time={item?.time} key={item.id}/>
 
     const alerts = alertItems?.map((item) => makeAlert(item), [])
 
-    return <div style={alertStyles.alertRoot}>{alerts}</div>
+    return <Box sx={customStyles.alertRoot}>{alerts}</Box>
 }
 
 export default AlertManager
